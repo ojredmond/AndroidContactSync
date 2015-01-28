5ed985c5767c5d74765aa8162667213e361d3476
ojredmond <oli@redmonds.me.uk>
2015-01-28 15:33:55 +0000

    Update SettingsFragment.java

--- app/src/main/java/uk/me/redmonds/contactsync/SettingsFragment.java
+++ app/src/main/java/uk/me/redmonds/contactsync/SettingsFragment.java
@@ -1,5 +1,12 @@
 package uk.me.redmonds.contactsync;
 
-public class SettingsFragment
-{
+public static class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
 }
