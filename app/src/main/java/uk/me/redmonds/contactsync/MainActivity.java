4709627a5c7b732223c2ff49e86ec360c1c477bb
ojredmond <oli@redmonds.me.uk>
2015-01-28 16:56:19 +0000

    Update MainActivity.java

--- app/src/main/java/uk/me/redmonds/contactsync/MainActivity.java
+++ app/src/main/java/uk/me/redmonds/contactsync/MainActivity.java
@@ -170,5 +170,11 @@ public class MainActivity extends ActionBarActivity
                 // Load the preferences from an XML resource
                 addPreferencesFromResource(R.layout.settings_fragment);
         }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
     }
 }
