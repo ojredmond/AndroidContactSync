5706a6ffa933d30eea58e298537fb9497ea3be5d
ojredmond <oli@redmonds.me.uk>
2015-01-29 16:19:20 +0000

    adding option settings fragment

---
+++ app/src/main/java/uk/me/redmonds/contactsync/SettingsFragment.java
@@ -0,0 +1,81 @@
+package uk.me.redmonds.contactsync;
+
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.layout.settings_fragment);
+
+        //get the account saved variable
+        ListPreference account1Pref = (ListPreference) findPreference("account1");
+        ListPreference account2Pref = (ListPreference) findPreference("account2");
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        AccountManager accounts = AccountManager.get(this.getActivity());
+
+        Account accountsList[] = accounts.getAccountsByType("com.google");
+        ArrayList<CharSequence> list = new ArrayList<CharSequence>();
+
+        for (Account a : accountsList) {
+            list.add(a.name);
+        }
+
+        CharSequence[] listArray = list.toArray(new String[list.size()]);
+
+        account1Pref.setEntries(listArray);
+        account1Pref.setEntryValues(listArray);
+
+        account2Pref.setEntries(listArray);
+        account2Pref.setEntryValues(listArray);
+
+        //set saved values to summary
+        ListPreference listPref = (ListPreference) account1Pref;
+        account1Pref.setSummary(listPref.getEntry());
+        listPref = (ListPreference) account2Pref;
+        account2Pref.setSummary(listPref.getEntry());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference pref = findPreference(key);
+
+        if (pref instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) pref;
+            pref.setSummary(listPref.getEntry());
+        }
+    }
+}
