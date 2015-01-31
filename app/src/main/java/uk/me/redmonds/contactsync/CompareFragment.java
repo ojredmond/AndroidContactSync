4d0cb527fe3a28ef7a26a0ab607fcfceb013439a
ojredmond <oli@redmonds.me.uk>
2015-01-31 22:39:57 +0000

    playing with compare

--- app/src/main/java/uk/me/redmonds/contactsync/CompareFragment.java
+++ app/src/main/java/uk/me/redmonds/contactsync/CompareFragment.java
@@ -1,82 +1,224 @@
 package uk.me.redmonds.contactsync;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.content.*;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.widget.*;
+import android.widget.TabHost.TabContentFactory;
+import android.view.*;
+import android.util.*;
 
-/**
- * Created by oli on 31/01/15.
- */
-public class CompareFragment extends android.app.Fragment {
-    // When requested, this adapter returns a DemoObjectFragment,
-    // representing an object in the collection.
-    DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
-    ViewPager mViewPager;
-    MainActivity main;
+public class CompareFragment extends android.app.Fragment
+{
+    private MainActivity main;
+    private ViewPager mViewPager;
+    private TabsAdapter mTabsAdapter;
+    private TabHost mTabsHost;
+    private int screenWidth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         main = (MainActivity)this.getActivity();
-        // ViewPager and its adapters use support library
-        // fragments, so use getSupportFragmentManager.
-        mDemoCollectionPagerAdapter =
-                new DemoCollectionPagerAdapter(
-                        main.getSupportFragmentManager());
-        mViewPager = (ViewPager) main.findViewById(R.id.pager);
-        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+        screenWidth = main.getWindowManager().getDefaultDisplay().getWidth();
+        mTabsHost = (TabHost)inflater.inflate(R.layout.tabs, container, false);
+        mTabsHost.setup();
+        mViewPager = (ViewPager)mTabsHost.findViewById(R.id.viewpager);
 
-        return mViewPager;
-    }
+        Bundle args = getArguments();
+        String listItem = args.getString("listItem");
+        String selected = args.getString("selected");
 
-    public class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter {
-        public DemoCollectionPagerAdapter(FragmentManager fm) {
-            super(fm);
+        //main.hideMenuIcon();
+        //main.getActionBar().hide();
+
+        mTabsAdapter = new TabsAdapter(main, mViewPager, mTabsHost, screenWidth);
+        SharedPreferences pref = main.getPreferences(Context.MODE_PRIVATE);
+        StringList list = new StringList(pref, listItem);
+        final TabWidget tabWidget = (TabWidget)((HorizontalScrollView)((LinearLayout)mTabsHost.getChildAt(0)).getChildAt(0)).getChildAt(0);
+
+        if (!list.equals(null)) {
+            SparseArray<String> contacts = list.getSparseArray();
+            for (int i=0; i<contacts.size(); i++)
+            {
+                String name = contacts.valueAt(i);
+                int id = contacts.keyAt(i);
+                Bundle argsDetail = new Bundle();
+                argsDetail.putString("listItem", listItem);
+                argsDetail.putString("name", name);
+                argsDetail.putInt("id", id);
+
+                TabHost.TabSpec tab = mTabsHost.newTabSpec(name).setIndicator(name);
+
+                if (listItem.startsWith(Match.UNMATCHNAMEKEY))
+                    mTabsAdapter.addTab(tab, MatchContact.class, argsDetail);
+                else
+                    mTabsAdapter.addTab(tab, CompareDetail.class, argsDetail);
+
+                //set the text colour of the tab
+                final TextView tv = (TextView) tabWidget.getChildAt(mTabsAdapter.getCount()-1).findViewById(android.R.id.title);
+                //tv.setTextColor(this.getResources().getColorStateList(R.color.));
+                tv.setFocusableInTouchMode(true);
+
+                if (name.equals(selected))
+                    mTabsHost.setCurrentTab(mTabsAdapter.getCount()-1);
+            }
         }
 
-        @Override
-        public Fragment getItem(int i) {
-            Fragment fragment = new DemoObjectFragment();
-            Bundle args = new Bundle();
-            // Our object is just an integer :-P
-            args.putInt(DemoObjectFragment.ARG_OBJECT, i + 1);
-            fragment.setArguments(args);
-            return fragment;
+        if (savedInstanceState != null) {
+            mTabsAdapter.onPageSelected(savedInstanceState.getInt("tab", 0));
+        }
+
+        return mTabsHost;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        View tab = ((TabWidget)((HorizontalScrollView)((LinearLayout)mTabsHost.getChildAt(0)).getChildAt(0)).getChildAt(0)).getChildAt(mTabsHost.getCurrentTab());
+        final TextView tv = (TextView) tab.findViewById(android.R.id.title);
+        tv.requestFocus();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt("tab", mTabsHost.getCurrentTab());
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * This is a helper class that implements the management of tabs and all
+     * details of connecting a ViewPager with associated TabHost.  It relies on a
+     * trick.  Normally a tab host has a simple API for supplying a View or
+     * Intent that each tab will show.  This is not sufficient for switching
+     * between pages.  So instead we make the content part of the tab host
+     * 0dp high (it is not shown) and the TabsAdapter supplies its own dummy
+     * view to show as the tab content.  It listens to changes in tabs, and takes
+     * care of switch to the correct paged in the ViewPager whenever the selected
+     * tab changes.
+     */
+    public static class TabsAdapter extends FragmentStatePagerAdapter
+            implements TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener
+    {
+        private final Context mContext;
+        private final TabHost mTabsHost;
+        private final ViewPager mViewPager;
+        private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+        private final int mScreenWidth;
+
+        static final class TabInfo {
+            private final Class<?> clss;
+            private final Bundle args;
+            private final Integer position;
+
+            TabInfo(Class<?> _class, Bundle _args, int pos) {
+                clss = _class;
+                args = _args;
+                position = pos;
+            }
+        }
+
+        /**
+         * A simple factory that returns dummy views to the Tabhost
+         * @author mwho
+         */
+        static final class TabFactory implements TabContentFactory {
+            private final Context mContext;
+
+            /**
+             * @param context
+             */
+            public TabFactory(Context context) {
+                mContext = context;
+            }
+
+            /** (non-Javadoc)
+             * @see android.widget.TabHost.TabContentFactory#createTabContent(java.lang.String)
+             */
+            public View createTabContent(String tag) {
+                View v = new View(mContext);
+                v.setMinimumWidth(0);
+                v.setMinimumHeight(0);
+                return v;
+            }
+        }
+
+
+        public TabsAdapter(FragmentActivity activity, ViewPager pager, TabHost tabs, int width) {
+            super(activity.getSupportFragmentManager());
+            mContext = activity;
+            mTabsHost = tabs;
+            mTabsHost.setOnTabChangedListener(this);
+            mViewPager = pager;
+            mViewPager.setAdapter(this);
+            mViewPager.setOnPageChangeListener(this);
+            mScreenWidth = width;
+        }
+
+        public void addTab(TabHost.TabSpec tab, Class<?> clss, Bundle args) {
+            TabInfo info = new TabInfo(clss, args, getCount());
+            //tab.setTag(info);
+            mTabs.add(info);
+            tab.setContent(new TabFactory(mContext));
+            mTabsHost.addTab(tab);
+            notifyDataSetChanged();
         }
 
         @Override
         public int getCount() {
-            return 100;
+            return mTabs.size();
         }
 
         @Override
-        public CharSequence getPageTitle(int position) {
-            return "OBJECT " + (position + 1);
+        public Fragment getItem(int position) {
+            TabInfo info = mTabs.get(position);
+            Fragment f = Fragment.instantiate(mContext, info.clss.getName(), info.args);
+            //f.setTag
+            return f;
         }
-    }
 
-    // Instances of this class are fragments representing a single
-// object in our collection.
-    public static class DemoObjectFragment extends Fragment {
-        public static final String ARG_OBJECT = "object";
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater,
-                                 ViewGroup container, Bundle savedInstanceState) {
-            // The last two arguments ensure LayoutParams are inflated
-            // properly.
-            View rootView = inflater.inflate(
-                    R.layout.fragment_collection_object, container, false);
-            Bundle args = getArguments();
-            ((TextView) rootView.findViewById(android.R.id.text1)).setText(
-                    Integer.toString(args.getInt(ARG_OBJECT)));
-            return rootView;
+        public void onPageSelected(int position) {
+            mTabsHost.setCurrentTab(position);
+        }
+
+        public void onPageScrollStateChanged(int state) {
+        }
+
+        public void onTabChanged(String p1)
+        {
+            mViewPager.setCurrentItem(mTabsHost.getCurrentTab());
+            int position = mTabsHost.getCurrentTab();
+            View tab = ((TabWidget)((HorizontalScrollView)((LinearLayout)mTabsHost.getChildAt(0)).getChildAt(0)).getChildAt(0)).getChildAt(position);
+            int tabWidth = tab.getMeasuredWidth();
+            float tabPos = tab.getX() - (mScreenWidth/2) + (tabWidth/2);
+
+            ((HorizontalScrollView)mTabsHost.findViewById(R.id.tabscroll)).smoothScrollTo(Math.round(tabPos), 0);
+        }
+
+        public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+            mViewPager.setCurrentItem(tab.getPosition());
+        }
+
+        public void onTabUnselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+        }
+
+        public void onTabReselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
         }
     }
 }
