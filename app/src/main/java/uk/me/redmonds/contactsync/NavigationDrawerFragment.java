fd6f272bce0a78b732f9a2860607c78ed1e91ba4
ojredmond <oli@redmonds.me.uk>
2015-02-02 15:23:40 +0000

    Update NavigationDrawerFragment.java

--- app/src/main/java/uk/me/redmonds/contactsync/NavigationDrawerFragment.java
+++ app/src/main/java/uk/me/redmonds/contactsync/NavigationDrawerFragment.java
@@ -101,7 +101,7 @@ public class NavigationDrawerFragment extends Fragment {
         });
         mDrawerListView.setAdapter(new ArrayAdapter<String>(
                 getActionBar().getThemedContext(),
-                R.layout.fragment_collection_object,
+                android.R.layout.simple_list_item_activated_1,
                 android.R.id.text1,
                 new String[]{
                         getString(R.string.title_settings),
