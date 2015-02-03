e64d67463296a69f41773a98693fbf86c926bdb8
ojredmond <oli@redmonds.me.uk>
2015-02-03 14:26:45 +0000

    Update MainActivity.java

--- app/src/main/java/uk/me/redmonds/contactsync/MainActivity.java
+++ app/src/main/java/uk/me/redmonds/contactsync/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.FragmentActivity;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.os.Bundle;
@@ -28,7 +29,7 @@ import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity
+public class MainActivity extends FragmentActivity
         implements StatusFragment.OnViewCreatedListener,
         NavigationDrawerFragment.NavigationDrawerCallbacks {
 
