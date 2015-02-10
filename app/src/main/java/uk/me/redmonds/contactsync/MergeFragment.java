c90b495a33637dac34fd04784a08187be593e14f
ojredmond <oli@redmonds.me.uk>
2015-02-10 11:31:39 +0000

    Update MergeFragment.java

--- app/src/main/java/uk/me/redmonds/contactsync/MergeFragment.java
+++ app/src/main/java/uk/me/redmonds/contactsync/MergeFragment.java
@@ -76,12 +76,12 @@ public class MergeFragment extends Fragment
                 contactHeading.setText(Contacts.getGroupName(type));
                 layout.addView(contactHeading);
                 for(HashMap<String,String> item: contact.get(type)) {
-                    if(item.get("data2") == null)
+                    if(item.get("data2") == null) {
                         TextView contactValue = (TextView)LayoutInflater.from(main)
                             .inflate(R.layout.list_row_1, layoutContainer, false);
                         contactValue.setText(item.get("data1"));
                         layout.addView(contactValue);
-                    else {
+                    } else {
                         GridLayout rowLayout = (GridLayout)LayoutInflater.from(main)
                             .inflate(R.layout.list_row_1, layoutContainer, false);
                         ((TextView)rowLayout.findViewById(R.id.value)).setText(item.get("data1"));
