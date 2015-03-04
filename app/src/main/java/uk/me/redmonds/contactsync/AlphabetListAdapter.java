6d11c3fc45cec87116b7015d92d5cdbeadd8a3da
ojredmond <oli@redmonds.me.uk>
2015-03-04 12:03:04 +0000

    Update AlphabetListAdapter.java

--- app/src/main/java/uk/me/redmonds/contactsync/AlphabetListAdapter.java
+++ app/src/main/java/uk/me/redmonds/contactsync/AlphabetListAdapter.java
@@ -31,9 +31,132 @@ public class AlphabetListAdapter extends BaseAdapter {
     }
     
     private List<Row> rows;
+    private int alphabetListLayout;
     
     public void setRows(List<Row> rows) {
         this.rows = rows;
+    }
+
+    AlphabetListAdapter (int aListLayout, ArrayList<String> Objects) {
+        alphabetListLayout = aListLayout;
+        this.rows = new ArrayList<Row>();
+        int start = 0;
+        int end = 0;
+        String previousLetter = null;
+        Object[] tmpIndexItem = null;
+        Pattern numberPattern = Pattern.compile("[0-9]");
+
+        for (String object : Objects) {
+            String firstLetter = object.substring(0, 1);
+
+            // Group numbers together in the scroller
+            if (numberPattern.matcher(firstLetter).matches()) {
+                firstLetter = "#";
+            }
+
+            // If we've changed to a new letter, add the previous letter to the alphabet scroller
+            if (previousLetter != null && !firstLetter.equals(previousLetter)) {
+                end = rows.size() - 1;
+                tmpIndexItem = new Object[3];
+                tmpIndexItem[0] = previousLetter.toUpperCase(Locale.UK);
+                tmpIndexItem[1] = start;
+                tmpIndexItem[2] = end;
+                alphabet.add(tmpIndexItem);
+
+                start = end + 1;
+            }
+
+            // Check if we need to add a header row
+            if (!firstLetter.equals(previousLetter)) {
+                rows.add(new Section(firstLetter));
+                sections.put(firstLetter, start);
+            }
+
+            // Add the country to the list
+            rows.add(new Item(object));
+            previousLetter = firstLetter;
+        }
+
+        if (previousLetter != null) {
+            // Save the last letter
+            tmpIndexItem = new Object[3];
+            tmpIndexItem[0] = previousLetter.toUpperCase(Locale.UK);
+            tmpIndexItem[1] = start;
+            tmpIndexItem[2] = rows.size() - 1;
+            alphabet.add(tmpIndexItem);
+        }
+        
+        updateAlphabetList();
+    }
+
+    public void updateAlphabetList() {
+        LinearLayout sideIndex = (LinearLayout) findViewById(alphabetListLayout);
+        sideIndex.removeAllViews();
+        indexListSize = alphabet.size();
+        if (indexListSize < 1) {
+            return;
+        }
+
+        int indexMaxSize = (int) Math.floor(sideIndex.getHeight() / 20);
+        int tmpIndexListSize = indexListSize;
+        while (tmpIndexListSize > indexMaxSize) {
+            tmpIndexListSize = tmpIndexListSize / 2;
+        }
+        double delta;
+        if (tmpIndexListSize > 0) {
+            delta = indexListSize / tmpIndexListSize;
+        } else {
+            delta = 1;
+        }
+
+        TextView tmpTV;
+        for (double i = 1; i <= indexListSize; i = i + delta) {
+            Object[] tmpIndexItem = alphabet.get((int) i - 1);
+            String tmpLetter = tmpIndexItem[0].toString();
+
+            tmpTV = new TextView(this);
+            tmpTV.setText(tmpLetter);
+            tmpTV.setGravity(Gravity.CENTER);
+            tmpTV.setTextSize(15);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+            tmpTV.setLayoutParams(params);
+            sideIndex.addView(tmpTV);
+        }
+
+        sideIndexHeight = sideIndex.getHeight();
+
+        sideIndex.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // now you know coordinates of touch
+                sideIndexX = event.getX();
+                sideIndexY = event.getY();
+
+                // and can display a proper item it country list
+                displayListItem();
+
+                return false;
+            }
+        });
+    }
+
+    public void displayListItem() {
+        LinearLayout sideIndex = (LinearLayout) findViewById(alphabetListLayout);
+        sideIndexHeight = sideIndex.getHeight();
+        // compute number of pixels for every side index item
+        double pixelPerIndexItem = (double) sideIndexHeight / indexListSize;
+
+        // compute the item index for given event position belongs to
+        int itemPosition = (int) (sideIndexY / pixelPerIndexItem);
+
+        // get the item (we can do it since we know item index)
+        if (itemPosition < alphabet.size()) {
+            Object[] indexItem = alphabet.get(itemPosition);
+            int subitemPosition = sections.get(indexItem[0]);
+
+            //ListView listView = (ListView) findViewById(android.R.id.list);
+            getListView().setSelection(subitemPosition);
+        }
     }
 
     @Override
